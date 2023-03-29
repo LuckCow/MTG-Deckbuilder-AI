3fbb5f9ab4073c879713ca782fdffe90a98e63e2
Nick Collins <LuckCow@users.noreply.github.com>
2023-03-29 15:48:13 -0400

    added max length for visualization instead of showing entire vocabulary

--- src/visualize_data.py
+++ src/visualize_data.py
@@ -14,13 +14,16 @@ val_values = np.load('../data/val_values.npy')
 
 enc = MTGStandardEncoder()
 
-def visualize_value_probs(prob_dist):
+def visualize_value_probs(prob_dist, max_cards=20):
     """Visualize the probability of each card in the value row. only show cards that have a non-zero probability"""
 
     # get indices of non-zero probabilities and sort them in descending order of probability
     nonzero_indices = np.where(prob_dist > 0)[0]
     sorted_indices = np.argsort(prob_dist[nonzero_indices])[::-1]
     sorted_nonzero_indices = nonzero_indices[sorted_indices]
+
+    # only show the top max_cards cards
+    sorted_nonzero_indices = sorted_nonzero_indices[:max_cards]
 
     # get the corresponding non-zero probabilities and outcomes
     nonzero_probs = prob_dist[sorted_nonzero_indices]
@@ -29,8 +32,6 @@ def visualize_value_probs(prob_dist):
     decode = lambda i: enc.decode(i) if len(enc.decode(i)) < 18 else enc.decode(i)[:18] + '-'
     outcomes = [decode(i) for i in sorted_nonzero_indices]
     #outcomes = [str(i) for i in sorted_nonzero_indices]
-
-    print(outcomes, nonzero_probs)
 
     # plot a bar chart of the non-zero probabilities
     fig = plt.figure(figsize=(8, 8))
